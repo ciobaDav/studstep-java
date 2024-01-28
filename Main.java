1ff5e36fe3086b0c66839bd2195bbda9d7e4de9d
Ciobanu David <davidciobanu06082006@gmail.com>
2024-01-28 12:54:06 +0200

    first commit

--- Main.java
+++ Main.java
@@ -1,7 +1,87 @@
-public class Main{
+	public class Main{
 	public static void main(String[] abcd){
 
-     
+  
+
+// 1.	Declarați o variabilă de tip int și atribuiți-i o valoare.
+        int i=5;
+
+
+// 2.	Declarați o variabilă de tip double și atribuiți-i o valoare.
+
+double number = 3.55;
+
+// 3.	Declarați o variabilă de tip boolean și atribuiți-i o valoare.
+
+boolean raspuns ;
+raspuns = true;
+raspuns = false;
+
+// 4.	Declarați o variabilă de tip String și atribuiți-i o valoare.
+String nume = "Ciobanu David";
+
+
+// 5.	Declarați două variabile int și efectuați o adunare între ele.
+int a=5 , b=6 , sum;
+ sum = a+b;
+System.out.println(sum);
+// 6.	Declarați două variabile int și efectuați o scădere între ele.
+int a=1450 , b=7 , diferenta;
+ diferenta = a-b;
+System.out.println(diferenta);
+
+// 7.	Declarați două variabile int și efectuați o înmulțire între ele.
+int a=12 , b=35,multiply;
+multiply = a*b;
+System.out.println(multiply);
+
+// 8.	Declarați două variabile int și efectuați o împărțire între ele.
+int a=12 , b=35,devide;
+double devide = a*b;
+System.out.println(devide);
+
+// 9.	Declarați o variabilă int și o variabilă double și efectuați o conversie de la int la double.
+int a=300;
+Double d = new Double(a);
+
+
+// 10.	Declarați o variabilă double și efectuați o conversie de la double la int.
+double data = 23.5567;
+int value = (int)data;
+System.out.println("Integer - " + data );
+
+// 11.	Declarați o variabilă de tip int și efectuați incrementarea cu 1.
+int a = 24;
+a++;
+System.out.println(a);
+// 12.	Declarați o variabilă de tip int și efectuați decrementarea cu 1.
+int a = 24;
+a--;
+System.out.println(a);
+
+// 13.	Declarați o variabilă de tip double și efectuați rotunjirea la întreg.
+double b = 23.55;
+round(b);
+System.out.println(b);
+
+
+// 14.	Declarați o variabilă de tip double și efectuați extragerea părții întregi in alta variabila.
+double nr = 11.25;
+int intnr = (int)Math; 
+round(nr);
+System.out.println(nr);
+
+
+// 15.	Declarați o variabilă de tip int și verificați dacă este pară sau impară.
+
+int a;
+if(a %2==0 ){
+	System.out.printl("Nr este par ")
+
+}else{
+	System.out.printl("Nr este impar ")
+}
+
 
 
 
